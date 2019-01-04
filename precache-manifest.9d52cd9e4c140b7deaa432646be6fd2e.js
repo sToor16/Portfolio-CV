8fda4a2019db8ef5615ba18779ca70689e1ce0d7
Toor <sstoor16@gmail.com>
2019-01-04 11:54:28 -0600

    Updates

---
+++ precache-manifest.9d52cd9e4c140b7deaa432646be6fd2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aec55bf832e16611774e27cb03a66faa",
+    "url": "/static/media/myPicure.aec55bf8.png"
+  },
+  {
+    "revision": "0b6f53274d74d5bb56ac5efcb1131bf1",
+    "url": "/static/media/contact_page_image.0b6f5327.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ab96bb917835eabcb5e",
+    "url": "/static/js/main.5ab96bb9.chunk.js"
+  },
+  {
+    "revision": "32ef56d9f992b656c098",
+    "url": "/static/js/1.32ef56d9.chunk.js"
+  },
+  {
+    "revision": "5ab96bb917835eabcb5e",
+    "url": "/static/css/main.a4ae06c2.chunk.css"
+  },
+  {
+    "revision": "32ef56d9f992b656c098",
+    "url": "/static/css/1.efbe9194.chunk.css"
+  },
+  {
+    "revision": "a81aa448555279ede110cf47b3f8370c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-cv/precache-manifest.a0aa9d1f99d01021435ac3d35e6b80c8.js"
+  "/precache-manifest.9d52cd9e4c140b7deaa432646be6fd2e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio-cv/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
